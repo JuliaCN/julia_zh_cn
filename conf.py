f55aeb51fb1343139b7197d23a0859f4b0c9e4f8
Lei WANG <kookbond@gmail.com>
2014-02-23 11:50:43 +0800

    try pdf

--- conf.py
+++ conf.py
@@ -186,21 +186,12 @@ latex_elements = {
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
-'utf8extra': r'''
-\usepackage{CJKutf8}
-\DeclareUnicodeCharacter{00A0}{\nobreakspace}
-\DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
-\DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
-\DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
-\DeclareUnicodeCharacter{2713}{x}
-\DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
-''',
 'preamble': '''
 \\hypersetup{unicode=true}
 \\usepackage{CJKutf8}
 \\begin{CJK}{UTF8}{gkai}
 \\AtEndDocument{\\end{CJK}}
-''',
+'''
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
