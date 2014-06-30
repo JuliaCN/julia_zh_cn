f6fd9e5e39611ab9d89d8afaf7eb8481776722e2
Yimin <GaZ3ll3@users.noreply.github.com>
2014-06-30 12:48:03 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -195,17 +195,19 @@ latex_elements = {
 'preamble': '''
 \\hypersetup{unicode=true}
 \\usepackage{CJKutf8}
-\\DeclareUnicodeCharacter{00A0}{\\nobreakspace}
-\\DeclareUnicodeCharacter{2203}{\\ensuremath{\\exists}}
-\\DeclareUnicodeCharacter{2200}{\\ensuremath{\\forall}}
-\\DeclareUnicodeCharacter{2286}{\\ensuremath{\\subseteq}}
-\\DeclareUnicodeCharacter{2713}{x}
-\\DeclareUnicodeCharacter{27FA}{\\ensuremath{\\Longleftrightarrow}}
-\\DeclareUnicodeCharacter{221A}{\\ensuremath{\\sqrt{}}}
-\\DeclareUnicodeCharacter{221B}{\\ensuremath{\\sqrt[3]{}}}
 \\begin{CJK}{UTF8}{gbsn}
 \\AtEndDocument{\\end{CJK}}
-'''
+''',
+'utf8extra': r'''
+\DeclareUnicodeCharacter{00A0}{\nobreakspace}
+\DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+\DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
+\DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
+\DeclareUnicodeCharacter{2713}{x}
+\DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
+\DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
+\DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
+''',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
